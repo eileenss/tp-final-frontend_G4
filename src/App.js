da8784cdc392b9d86497cf013240c508b1c0c604
lucasmoix <129341492+lucasmoix@users.noreply.github.com>
2023-11-13 22:04:46 -0300

    user-register

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import RegisterPage from './components/User/RegisterPage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="aplicacion">
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element= {<h1>Home</h1>} />
+          <Route path='/register' element= {<RegisterPage/>} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
