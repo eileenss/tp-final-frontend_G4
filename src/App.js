e788335bea4d4b6d9acb9dedb320f4a903b8ff28
Matias Quesada <matiasquesada00@gmail.com>
2023-11-14 14:35:02 -0300

    libDet

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Libros from "./components/libros/Libros";
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Libros from './components/libros/Libros';
+import LibroDetails from './components/libros/LibroDetails';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/libros" element={<Libros />} />
+        <Route path="/libros/:id" element={<LibroDetails />} />
       </Routes>
     </BrowserRouter>
   );
--- src/components/libros/Libro.jsx
+++ src/components/libros/Libro.jsx
@@ -1,18 +1,22 @@
+import { Link } from 'react-router-dom';
+
 const Libro = (props) => {
   return (
     <li>
       <div>
-        <div>
-          <img src={props.Imagen} alt="Tapa de libro"></img>
-        </div>
-        <h1>Título: {props.Titulo}</h1>
-        <h3>Autor: {props.Autor}</h3>
-        <h3>Género: {props.Genero}</h3>
-        <h3>Sinopsis: {props.Sinopsis}</h3>
-        <h3>Fecha de publicación: {props.FechaPublicacion}</h3>
-        <h3>Editorial: {props.Editorial}</h3>
-        <h3>Idioma: {props.Idioma}</h3>
-        <h3>Estado: {props.Estado}</h3>
+        <Link to={`/libros/${props.Id}`}>
+          <div>
+            <img src={props.Imagen} alt="Tapa de libro"></img>
+          </div>
+          <h1>Título: {props.Titulo}</h1>
+          <h3>Autor: {props.Autor}</h3>
+          <h3>Género: {props.Genero}</h3>
+          <h3>Sinopsis: {props.Sinopsis}</h3>
+          <h3>Fecha de publicación: {props.FechaPublicacion}</h3>
+          <h3>Editorial: {props.Editorial}</h3>
+          <h3>Idioma: {props.Idioma}</h3>
+          <h3>Estado: {props.Estado}</h3>
+        </Link>
       </div>
     </li>
   );
