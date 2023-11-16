8001a0e133f235319c34d7ec685fc4ed5ab7ded3
leandro rodriguez <leandro.h.rodriguez@gmail.com>
2023-11-16 20:49:46 -0300

    mejora link libro

--- src/components/libros/Libro.jsx
+++ src/components/libros/Libro.jsx
@@ -9,17 +9,12 @@ const Libro = (props) => {
             <img src={props.Imagen} alt="Tapa de libro"></img>
           </div>
           <h1>Título: {props.Titulo}</h1>
-          <h3>Autor: {props.Autor}</h3>
-          <h3>Género: {props.Genero}</h3>
-          <h3>Sinopsis: {props.Sinopsis}</h3>
-          <h3>Fecha de publicación: {props.FechaPublicacion}</h3>
-          <h3>Editorial: {props.Editorial}</h3>
-          <h3>Idioma: {props.Idioma}</h3>
-          <h3>Estado: {props.Estado}</h3>
         </Link>
+        <h3>Autor: {props.Autor}</h3>
+        <h3>Estado: {props.Estado}</h3>
       </div>
     </li>
-  );
+  ); 
 };
 
 export default Libro;
