0b2a5893818bcbb07aa8bf637a168d7703a8579b
leandro rodriguez <leandro.h.rodriguez@gmail.com>
2023-11-16 21:21:47 -0300

    mejora link libro2

--- src/components/libros/Libro.jsx
+++ src/components/libros/Libro.jsx
@@ -1,3 +1,5 @@
+import './styles.css';
+
 import { Link } from 'react-router-dom';
 
 const Libro = (props) => {
--- src/components/libros/LibroDetails.jsx
+++ src/components/libros/LibroDetails.jsx
@@ -1,5 +1,6 @@
 import { useParams } from 'react-router-dom';
 import { useState, useEffect } from 'react';
+import './styles.css';
 
 const LibroDetails = (props) => {
   const { id } = useParams();
