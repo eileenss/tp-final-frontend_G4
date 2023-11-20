49ecbcb3adfd9d119a53dad483795a7182b69a5e
leandro rodriguez <leandro.h.rodriguez@gmail.com>
2023-11-20 16:37:55 -0300

    funcionaliadad_alquilar_devolver

--- src/components/libros/ListadoLibros.jsx
+++ src/components/libros/ListadoLibros.jsx
@@ -1,22 +1,27 @@
-import Libro from "./Libro";
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 const ListadoLibros = (props) => {
   return (
     <ul>
       {props.Libros.map((libro) => {
+        const estadoClass = libro.Estado === 'Disponible' ? 'estado-disponible' : 'estado-alquilado';
+
         return (
-          <Libro
-            Id={libro._id}
-            Titulo={libro.Titulo}
-            Autor={libro.Autor}
-            Genero={libro.Genero}
-            Sinopsis={libro.Sinopsis}
-            FechaPublicacion={libro["Fecha-publicacion"]}
-            Editorial={libro.Editorial}
-            Idioma={libro.Idioma}
-            Estado={libro.Estado}
-            Imagen={libro.Imagen}
-          />
+          <li key={libro._id}>
+            <div className={estadoClass}>
+              <Link to={`/libros/${libro._id}`}>
+                <div>
+                  <img src={libro.Imagen} alt="Tapa de libro" />
+                </div>
+                <h1>{libro.Titulo}</h1>
+              </Link>
+              <h3>{libro.Autor}</h3>
+              <h3>
+                Estado: {libro.Estado === 'Disponible' ? 'Disponible' : <span className={estadoClass}>Alquilado</span>}
+              </h3>
+            </div>
+          </li>
         );
       })}
     </ul>
