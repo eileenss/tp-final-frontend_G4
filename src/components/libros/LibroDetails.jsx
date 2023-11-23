caf0ca1bc4abf089111066a4d1f6869367c2dc09
Matias Quesada <matiasquesada00@gmail.com>
2023-11-23 16:29:34 -0300

    esto no anda

--- src/components/libros/LibroDetails.jsx
+++ src/components/libros/LibroDetails.jsx
@@ -1,10 +1,12 @@
 import { useParams } from 'react-router-dom';
 import { useState, useEffect } from 'react';
+import { jwtDecode } from 'jwt-decode';
 import './styles.css';
 
 const LibroDetails = (props) => {
   const { id } = useParams();
   const [libro, setLibro] = useState({});
+  const [userId, setUserId] = useState(null);
 
   useEffect(() => {
     fetch(`http://localhost:4000/libros/${id}`)
@@ -15,28 +17,60 @@ const LibroDetails = (props) => {
       .catch((error) => console.log('Error en la carga', error));
   }, [id]);
 
+  const token = localStorage.getItem('token');
+
+  useEffect(() => {
+    const obtenerUserId = async () => {
+      if (token) {
+        try {
+          const decodedToken = jwtDecode(token);
+          const userId = decodedToken._id;
+          setUserId(userId);
+        } catch (error) {
+          console.error('Error al decodificar el token:', error);
+        }
+      }
+    };
+
+    obtenerUserId();
+  }, [token]);
+
   const alquilarLibro = async () => {
     try {
-      const response = await fetch(`http://localhost:4000/libros/alquilar/${id}`, {
-        method: 'POST',
-      });
+      const response = await fetch(
+        `http://localhost:4000/libros/alquilar/${id}`,
+        {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ id, userId }),
+        }
+      );
 
       if (response.ok) {
         setLibro((prevLibro) => ({ ...prevLibro, Estado: 'Alquilado' }));
         console.log('Libro alquilado con éxito.');
       } else {
-        console.error('Error al alquilar el libro.');
+        console.error('Error al alquilar el libroo.');
       }
     } catch (error) {
       console.error('Error de red:', error);
     }
   };
 
-const devolverLibro = async () => {
+  const devolverLibro = async () => {
     try {
-      const response = await fetch(`http://localhost:4000/libros/devolver/${id}`, {
-        method: 'POST',
-      });
+      const response = await fetch(
+        `http://localhost:4000/libros/devolver/${id}`,
+        {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ userId }),
+        }
+      );
 
       if (response.ok) {
         setLibro((prevLibro) => ({ ...prevLibro, Estado: 'Disponible' }));
@@ -49,7 +83,8 @@ const devolverLibro = async () => {
     }
   };
 
-  const estadoClass = libro.Estado === 'Disponible' ? 'estado-disponible' : 'estado-alquilado';
+  const estadoClass =
+    libro.Estado === 'Disponible' ? 'estado-disponible' : 'estado-alquilado';
 
   return (
     <div className="LibroDetails-container">
@@ -63,12 +98,17 @@ const devolverLibro = async () => {
       <h3>Fecha de publicación: {libro.FechaPublicacion}</h3>
       <h3>Editorial: {libro.Editorial}</h3>
       <h3>Idioma: {libro.Idioma}</h3>
-      <h3>Estado: <span className={estadoClass}>{libro.Estado}</span></h3>
-      {libro.Estado === 'Disponible' && <button onClick={alquilarLibro}>Alquilar</button>}
-      {libro.Estado === 'Alquilado' && <button onClick={devolverLibro}>Devolver</button>}
+      <h3>
+        Estado: <span className={estadoClass}>{libro.Estado}</span>
+      </h3>
+      {libro.Estado === 'Disponible' && (
+        <button onClick={alquilarLibro}>Alquilar</button>
+      )}
+      {libro.Estado === 'Alquilado' && (
+        <button onClick={devolverLibro}>Devolver</button>
+      )}
     </div>
   );
 };
 
 export default LibroDetails;
-
