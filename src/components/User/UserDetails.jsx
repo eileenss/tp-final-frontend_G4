a0f0bba15d1a5a4de8131439760ff62cef6edcb7
Matias Quesada <matiasquesada00@gmail.com>
2023-11-23 21:37:25 -0300

    devolverLibro

--- src/components/User/UserDetails.jsx
+++ src/components/User/UserDetails.jsx
@@ -1,78 +1,97 @@
-import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
-import Libro from "../libros/Libro";
-import { Link } from "react-router-dom";
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import Libro from '../libros/Libro';
+import { Link } from 'react-router-dom';
+import { jwtDecode } from 'jwt-decode';
 
 const UserDetails = () => {
-    const { id } = useParams();
-    const [user, setUser] = useState([]);
-    const [librosDetails, setLibrosDetails] = useState([]);
-    const [libro, setLibro] = useState([]);
-    
-    useEffect(() => {
-        fetch(`http://localhost:4000/users/user/${id}`)
-            .then(response => response.json())
-            .then(data => setUser(data))
-            .catch(error => console.log(error));
-    }, [id]);
+  const { id } = useParams();
+  const [user, setUser] = useState([]);
+  const [librosDetails, setLibrosDetails] = useState([]);
+  const [libro, setLibro] = useState([]);
+  const token = localStorage.getItem('token');
 
-    useEffect(() => {
-        if (user && user.libros) {
-            const fetchLibrosDetails = async () => {
-              const librosPromises = user.libros.map(libroId =>
-                fetch(`http://localhost:4000/libros/${libroId}`)
-                  .then(response => response.json())
-              );
-              const librosData = await Promise.all(librosPromises);
-              setLibrosDetails(librosData);
-            };
-            fetchLibrosDetails();
-          }
-      }, [user]);
+  useEffect(() => {
+    fetch(`http://localhost:4000/users/user/${id}`)
+      .then((response) => response.json())
+      .then((data) => setUser(data))
+      .catch((error) => console.log(error));
+  }, [id]);
 
-    const devolverLibro = async (id) => {
-      try {
-        const response = await fetch(`http://localhost:4000/libros/devolver/${id}`, {
+  useEffect(() => {
+    if (user && user.libros) {
+      const fetchLibrosDetails = async () => {
+        const librosPromises = user.libros.map((libroId) =>
+          fetch(`http://localhost:4000/libros/${libroId}`).then((response) =>
+            response.json()
+          )
+        );
+        const librosData = await Promise.all(librosPromises);
+        setLibrosDetails(librosData);
+      };
+      fetchLibrosDetails();
+    }
+  }, [user]);
+
+  const devolverLibro = async (idLibro) => {
+    console.log(idLibro);
+    const idUser = jwtDecode(token)._id;
+    try {
+      const responseLibro = await fetch(
+        `http://localhost:4000/libros/devolver/${idLibro}`,
+        {
           method: 'POST',
-        });
-  
-        if (response.ok) {
-          setLibro((prevLibro) => ({ ...prevLibro, Estado: 'Disponible' }));
-          console.log('Libro devuelto con éxito.');
-        } else {
-          console.error('Error al devolver el libro.');
         }
-      } catch (error) {
-        console.error('Error de red:', error);
-      }
-        // Lógica para devolver el libro con el ID bookId
-        //console.log(`Devolviendo libro con ID: ${bookId}`);
-        // Aquí puedes realizar una solicitud al servidor para devolver el libro, o realizar la lógica necesaria.
-    };
-
-    return (
-        <div>
-            <div>
-              <h2>Mis Libros</h2>
-              <button><Link to="/">Ir a la página de inicio</Link></button>
-              <p>DNI: {user.dni}</p>
-              <ul>
-                {librosDetails.map((libroDetails, index) => (
-                  <li key={libroDetails._id}>
-                    <Libro
-                      Imagen={libroDetails.Imagen}
-                      Titulo={libroDetails.Titulo}
-                      Autor={libroDetails.Autor}
-                      Estado={libroDetails.Estado}
-                      mostrarEstado={false}
-                    />
-                    <button onClick={() => devolverLibro(user.libros[index])}>Devolver</button>
-                  </li>
-                ))}
-              </ul>
-            </div>
-        </div>
       );
-    };
-    
-    export default UserDetails;
+      const responseUsuario = await fetch(
+        `http://localhost:4000/users/devolver/`,
+        {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ idLibro, idUser }),
+        }
+      );
+
+      if (responseLibro.ok) {
+        setLibro((prevLibro) => ({ ...prevLibro, Estado: 'Disponible' }));
+        console.log('Libro devuelto con éxito.');
+      } else {
+        console.error('Error al devolver el libro.');
+      }
+    } catch (error) {
+      console.error('Error de red:', error);
+    }
+  };
+
+  return (
+    <div>
+      <div>
+        <h2>Mis Libros</h2>
+        <button>
+          <Link to="/">Ir a la página de inicio</Link>
+        </button>
+        <p>DNI: {user.dni}</p>
+        <ul>
+          {librosDetails.map((libroDetails, index) => (
+            <li key={libroDetails._id}>
+              <Libro
+                Imagen={libroDetails.Imagen}
+                Titulo={libroDetails.Titulo}
+                Autor={libroDetails.Autor}
+                Estado={libroDetails.Estado}
+                mostrarEstado={false}
+              />
+              <button onClick={() => devolverLibro(libroDetails._id)}>
+                Devolver
+              </button>{' '}
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default UserDetails;
--- src/components/libros/LibroDetails.jsx
+++ src/components/libros/LibroDetails.jsx
@@ -3,7 +3,6 @@ import { useState, useEffect } from 'react';
 import './styles.css';
 import { jwtDecode } from 'jwt-decode';
 
-
 const LibroDetails = (props) => {
   const token = localStorage.getItem('token');
   const { id } = useParams();
@@ -19,49 +18,38 @@ const LibroDetails = (props) => {
   }, [id]);
 
   const alquilarLibro = async (idLibro) => {
-  const idUser = jwtDecode(token)._id;
-  try {
-    const responseLibro = await fetch(`http://localhost:4000/libros/alquilar/${id}`, {
-      method: 'POST',
-    });
-    const responseUsuario = await fetch(`http://localhost:4000/users/alquilar/`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({id, idUser}),
-    });
-
-    if (responseLibro.ok) {
-      setLibro((prevLibro) => ({ ...prevLibro, Estado: 'Alquilado' }));
-      console.log('Libro alquilado con éxito.');
-    } else {
-      console.error('Error al alquilar el libro.');
-    }
-  } catch (error) {
-    console.error('Error de red:', error);
-  }
-};
-
-const devolverLibro = async () => {
-  const idUsuario = jwtDecode(token)._id
+    const idUser = jwtDecode(token)._id;
     try {
-      const response = await fetch(`http://localhost:4000/libros/devolver/${id}`, {
-        method: 'POST',
-      });
+      const responseLibro = await fetch(
+        `http://localhost:4000/libros/alquilar/${id}`,
+        {
+          method: 'POST',
+        }
+      );
+      const responseUsuario = await fetch(
+        `http://localhost:4000/users/alquilar/`,
+        {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ id, idUser }),
+        }
+      );
 
-      if (response.ok) {
-        setLibro((prevLibro) => ({ ...prevLibro, Estado: 'Disponible' }));
-        console.log('Libro devuelto con éxito.');
+      if (responseLibro.ok) {
+        setLibro((prevLibro) => ({ ...prevLibro, Estado: 'Alquilado' }));
+        console.log('Libro alquilado con éxito.');
       } else {
-        console.error('Error al devolver el libro.');
+        console.error('Error al alquilar el libro.');
       }
     } catch (error) {
       console.error('Error de red:', error);
     }
   };
 
-  const estadoClass = libro.Estado === 'Disponible' ? 'estado-disponible' : 'estado-alquilado';
+  const estadoClass =
+    libro.Estado === 'Disponible' ? 'estado-disponible' : 'estado-alquilado';
 
   return (
     <div className="LibroDetails-container">
@@ -75,12 +63,14 @@ const devolverLibro = async () => {
       <h3>Fecha de publicación: {libro.FechaPublicacion}</h3>
       <h3>Editorial: {libro.Editorial}</h3>
       <h3>Idioma: {libro.Idioma}</h3>
-      <h3>Estado: <span className={estadoClass}>{libro.Estado}</span></h3>
-      {libro.Estado === 'Disponible' && <button onClick={alquilarLibro}>Alquilar</button>}
-      {libro.Estado === 'Alquilado' && <button onClick={devolverLibro}>Devolver</button>}
+      <h3>
+        Estado: <span className={estadoClass}>{libro.Estado}</span>
+      </h3>
+      {libro.Estado === 'Disponible' && (
+        <button onClick={alquilarLibro}>Alquilar</button>
+      )}
     </div>
   );
 };
 
 export default LibroDetails;
-
